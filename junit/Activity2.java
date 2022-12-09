c8b6a68a37b82fb89050e803d19eb050fa5c36de
sushma-Gitacc <77955574+sushma-Gitacc@users.noreply.github.com>
2022-12-09 15:01:42 +0530

    Add files via upload

---
+++ junit/Activity2.java
@@ -0,0 +1,21 @@
+package Activities;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+public class Activity2 {
+
+    @Test
+    public void notEnoughFunds(){
+        BankAccount account=new BankAccount(9);
+        assertThrows(NotEnoughFundsException.class, ()-> account.withdraw(10));
+
+    }
+    @Test
+    public void enoughFunds(){
+        BankAccount account = new BankAccount(100);
+        assertDoesNotThrow(() -> account.withdraw(100));
+    }
+}
